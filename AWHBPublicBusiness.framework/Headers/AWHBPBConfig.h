d28448c1b79408388618382db83703ef9fdba684
王恒 <1066026709@qq.com>
2022-10-14 00:11:51 +0800

    更新版本

---
+++ AWHBPublicBusiness.framework/Headers/AWHBPBConfig.h
@@ -0,0 +1,18 @@
+//
+//  AWHBPBConfig.h
+//  AWHBPublicBusiness
+//
+//  Created by 王恒 on 2022/10/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWHBPBConfig : NSObject
+
++ (void)setup;
+
+@end
+
+NS_ASSUME_NONNULL_END
