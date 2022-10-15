d5b76b63e86df5188d6c895243cb6c13c518a1e6
王恒 <1066026709@qq.com>
2022-10-15 09:22:14 +0800

    开发公共类，解决编译错误

---
+++ AWHBPublicBusiness.framework/Headers/AWHBPBHistoryVideoTypeCell.h
@@ -0,0 +1,19 @@
+//
+//  AWHBPBHistoryVideoTypeCell.h
+//  AWHBPublicBusiness
+//
+//  Created by 王恒 on 2022/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AWHBPBHistoryVideoTypeCell : UITableViewCell
+
+@property (nonatomic,strong)UILabel *titleLab;
+@property (nonatomic,strong)UIImageView *desImageView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AWHBPublicBusiness.framework/Headers/AWHBPBHistoryVideoTypeView.h
@@ -0,0 +1,22 @@
+//
+//  AWHBPBHistoryVideoTypeView.h
+//  AWHBPublicBusiness
+//
+//  Created by 王恒 on 2022/9/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+typedef void(^SelectBlock)(NSString *string,NSString *placeLabText);
+@interface AWHBPBHistoryVideoTypeView : UIView<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,strong)UITableView *tableView;
+@property(nonatomic,strong)NSMutableArray *dataArray;
+@property(nonatomic,strong)UILabel *placeLab;
+@property(nonatomic,copy)NSString *name;
+@property(nonatomic,strong)NSString *selectName;
+@property(nonatomic,copy)SelectBlock SelectBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END
